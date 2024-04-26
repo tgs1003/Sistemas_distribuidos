9943bfa09eb6d5baaf77f0da049e5697410afd91
Ricardo García <tgs1003@alu.ubu.es>
2024-04-26 10:42:20 +0200

    Borrar archivos sobrantes.

--- Practica_OB2/Practica2ChatRMI/registro.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-export CLASSPATH=.\target\Practica2ChatRMI-0.0.1-SNAPSHOT.jar
-
-rmiregistry -J-Djava.rmi.server.codebase=http://localhost:8080/Practica2ChatRMI-Web/
--- Practica_OB2/Practica2ChatRMI/servidor.sh
+++
@@ -1,7 +0,0 @@
-export CLASSPATH=.\target\Practica2ChatRMI-0.0.1-SNAPSHOT.jar
-
-java -Djava.security.policy=registerit.policy ^
--Djava.rmi.server.codebase=http://localhost:8080/Practica2ChatRMI-Web/ ^
-es.ubu.lsi.server.ChatServerDynamic
-
-read -p "Pulse una tecla para continuar..."
